54ddf66a23cd17fa29cc56c20310d2c44328aa24
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-03-05 20:35:31 +0900

    http2サーバーをdockerで

--- echo-http/server.go
+++ echo-http/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -8,8 +10,18 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	e.GET("/request", func(c echo.Context) error {
+		req := c.Request()
+		format := `
+		Protocol: %s<br>
+		Host: %s<br>
+		Remote Address: %s<br>
+		Method: %s<br>
+		Path: %s<br>
+		`
+		return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	if err := e.StartTLS(":8080", "cert.pem", "key.pem"); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
